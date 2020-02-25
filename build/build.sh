10424895a90b5d923ea01b041b513faa7a996ec6
remymess <remy.messadene@gmail.com>
2020-02-25 18:10:25 +0000

    Updating build script

--- build/build.sh
+++ build/build.sh
@@ -8,9 +8,9 @@ if ! /usr/bin/git pull; then
     exit
 fi
 
-if ! [ -d ".env" ]; then
-    virtualenv -p python3 ../..env
+if ! [ -d ".env_sutton" ]; then
+    virtualenv -p python3 ../.env_sutton
 fi
-source ../.env/bin/activate
+source ../.env_sutton/bin/activate
 
 pip install -r requirements.txt
