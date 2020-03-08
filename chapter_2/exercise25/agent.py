87fd2297e2403352bf5c9aef873334a839a01e96
remymess <remy.messadene@gmail.com>
2020-03-08 01:44:16 +0000

    Implemented exercise; to quickly check: BM update is good

--- chapter_2/exercise25/agent.py
+++ chapter_2/exercise25/agent.py
@@ -1,58 +1,86 @@
-from test_bed_env import TestBedEnv
 import numpy as np
 import logging
+import random
+import matplotlib.pyplot as plt
 
-# Meta variables
-ENV_NUMBER_TEST_BEDS = 100
-
-
-class ActionValueAgent(TestBedEnv):
+class ActionValueAgent:
     """
     Agent working using action-value algorithm.
     """
 
-    def __init__(self, n_it, n_bed, type_learning_rate, alpha_cst = 1):
-        self.n_it = n_it
-        self.env = TestBedEnv(ENV_NUMBER_TEST_BEDS)
-        self.action_values = np.zeros(ENV_NUMBER_TEST_BEDS)
-        self.estimate_actions = np.zeros(ENV_NUMBER_TEST_BEDS)
-        self.env = TestBedEnv(n_bed)
+    def __init__(self, n_bed, type_learning_rate, epsilon = 0.1, alpha_cst = 1):
+        self.n_bed = n_bed
+        self.action_values = np.zeros(n_bed)
+        self.estimate_actions = np.zeros(n_bed)
+        self.action_estimation_init = list(range(n_bed))
+        self.n_it = 1
+        self.type_learning_rate = type_learning_rate
+        self.epsilon = epsilon
+
+        self.optimal_action_number_tracker = 0
+        self.optimal_action_ratio_tracker = []
 
         assert(isinstance(type_learning_rate, str))
         if type_learning_rate not in ["sample", "constant"]:
             logging.error("__init__: variable 'type_learning_rate' must be either 'sample' or 'constant'.")
         else:
             if type_learning_rate == "sample":
-                def alpha(self, n):
+                def alpha():
                     """
                     Learning rate alpha in the action-value update.
                     """
-                    if n > 0:
-                        return 1/n
-                    else:
-                        raise AssertionError("alpha: n must be an integer.")
+                    return 1 / self.n_it
             else:
-                def alpha(self, n):
+                def alpha():
                     """
                     Learning rate alpha in the action-value update.
                     """
                     return alpha_cst
+
+            setattr(self, "alpha", alpha)
         
-    def select_action():
-        """
-        Select action based on best estiaate.
-        """
-        # get higher action
-        argmax = argmax(self.estimate_actions)
-        candidates = []
-        for est in self.estimate_actions:
-            if est > max:
-                est = max
-                candidates = []
-            elif est == max:
-                if                
+    def alpha(self):
+        raise NotImplementedError()
 
+    def select_action(self):
+        """
+        Select action based on best estimate with probaility 1-epsilon.
+        """
+        # check if all values have been tried.
+        if len(self.action_estimation_init) == 0:
+            # epsilon test for randomly selected action
+            _will_select_random = random.choices([True, False], (self.epsilon, 1 - self.epsilon))[0]
 
-    def train(learning_rate):
-        for _ in range(n_it):
-            
+            if _will_select_random:
+                return random.choices(list(range(len(self.estimate_actions))))
+            else:
+                max_rew = self.estimate_actions[0]
+                candidates = [0]
+                for i in range(1, len(self.estimate_actions)):
+                    if self.estimate_actions[i] < max_rew:
+                        continue
+                    elif self.estimate_actions[i] == max_rew:
+                        candidates.append(i)
+                    elif self.estimate_actions[i] > max_rew:
+                        max_rew = self.estimate_actions[i]
+                        candidates = [i]
+                    
+                best = random.choice(candidates)
+                self.n_it += 1
+                return best
+
+        else:
+            pick = random.choice(list(range(len(self.action_estimation_init))))
+            action = self.action_estimation_init[pick]
+            del self.action_estimation_init[pick]
+            self.n_it += 1
+            return action
+    
+    def update(self, action, reward):
+        self.estimate_actions[action] = reward + self.alpha() * (self.estimate_actions[action] - reward)
+
+    def plot_optimality_actions(self):
+        plt.plot(self.optimal_action_ratio_tracker)
+        plt.title(f"Agent with epsilon {self.epsilon} and {self.type_learning_rate} learning rate")
+        plt.show()
+
--- chapter_2/exercise25/test_bed_env.py
+++ chapter_2/exercise25/test_bed_env.py
@@ -1,7 +1,9 @@
 import numpy as np
 import random
 
+# Meta variables
 random.seed = 1
+ENV_NUMBER_TEST_BEDS = 100
 
 class TestBedEnv:
     """
@@ -9,40 +11,76 @@ class TestBedEnv:
     Initialises all means with a similiar random value between 0 and 1, they then
     evolves according to a random walk.
     """
-    def __init__(self, n_bed, low_b_mean=0, high_b_mean=1, vars=1):
+    def __init__(self, n_bed=ENV_NUMBER_TEST_BEDS, low_b_mean=0, high_b_mean=1, vars=1):
         self.n_bed = n_bed
-
         self.means = np.ones(n_bed) * (random.random() * (high_b_mean - low_b_mean) + low_b_mean)
         self.vars = np.ones(n_bed) * vars
 
-    def _update_mean(self, var=1, mean=0):
+    def _update_env(self, var=1, mean=0):
         """
         Update means according to independant random walks
         """
-        self.means += random.gauss(mean, var) * np.ones(self.n_bed)
+        self.means += np.random.normal(mean, var, self.n_bed)
 
-    def get_rewards(self, bed_number):
+    def optimal_actions(self):
+        """
+        Return a list of the best actions.
+        """
+        max_mean = np.max(self.means)
+        candidates = []
+        for i in range(self.n_bed):
+            if self.means[i] == max_mean:
+                candidates.append(i)
+                print(self.means, max_mean, candidates)
+
+        return candidates
+
+    def track_optimal_action(self, agent, suggested_action):
+        if suggested_action in self.optimal_actions():
+            agent.optimal_action_number_tracker += 1
+
+        agent.optimal_action_ratio_tracker.append(agent.optimal_action_number_tracker / agent.n_it)
+
+    def get_rewards(self, action):
         # return list of reward per action
-        rew = np.random.normal(self.means, self.vars, self.n_bed)
-
-        # update the mean according to bm
-        self._update_mean()
-        
-        return rew
+        return np.random.normal(self.means[action], self.vars[action])
 
     def response(self, action):
-        return 1
+        # get reward
+        rew = self.get_rewards(action)
 
-    def run(self, agent):
-        # agent takes action
-        action = agent.select_action()
+        # update environment
+        self._update_env()
 
-        # environment responds
-        rew = self.response(action)
+        return rew
 
-        # agent updates his memory
-        agent.update(action, rew)
+    def run(self, agent, n_it = 10000):
+        for _ in range(n_it):
+            # agent takes action
+            action = agent.select_action()
 
+            # track optimality
+            self.track_optimal_action(agent, action)
+
+            # environment responds
+            rew = self.response(action)
+
+            # agent updates his memory
+            agent.update(action, rew)
+
+        agent.plot_optimality_actions()
 
 if __name__ == "__main__":
-    # testing unit
+    # Init
+    from agent import ActionValueAgent
+    ENV_NUMBER_TEST_BEDS = 10
+    n_it = 1000
+
+    agent_cst = ActionValueAgent(n_bed=ENV_NUMBER_TEST_BEDS, type_learning_rate="constant", alpha_cst=0.05)
+    agent_sample_av = ActionValueAgent(n_bed=ENV_NUMBER_TEST_BEDS, type_learning_rate="sample", alpha_cst=0.05)
+    
+    env = TestBedEnv(n_bed=ENV_NUMBER_TEST_BEDS)
+
+    # Run simulation
+    for agent in [agent_cst, agent_sample_av]:
+        env.run(agent_cst)
